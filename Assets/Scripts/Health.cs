57156c2d83bbd173439df94bb3d439281f788c78
Irons-A <120574631+Irons-A@users.noreply.github.com>
2025-01-09 12:17:13 +0300

    v1

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _minHealth = 0;
+    [SerializeField] private float _maxHealth = 100;
+    [SerializeField] private float _currentHealth = 100;
+    [SerializeField] private float _damageAmount = 10;
+    [SerializeField] private float _healingAmount = 10;
+
+    [SerializeField] private HealthbarText _healthText;
+    [SerializeField] private HealthbarSlider _healthbarStandart;
+    [SerializeField] private HealthbarSlider _healthbarSmooth;
+
+    [SerializeField] private Button _damageButton;
+    [SerializeField] private Button _healButton;
+
+    private void Awake()
+    {
+        _damageButton.onClick.AddListener(TakeDamage);
+        _healButton.onClick.AddListener(Heal);
+    }
+
+    private void Start()
+    {
+        _healthText.SetMaxHealth(_maxHealth);
+        _healthbarStandart.SetStartParameters(_minHealth, _maxHealth);
+        _healthbarSmooth.SetStartParameters(_minHealth, _maxHealth);
+
+        DisplayHealth();
+    }
+
+    private void DisplayHealth()
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth, _minHealth, _maxHealth);
+
+        _healthText.SetHealth(_currentHealth);
+        _healthbarStandart.SetHealth(_currentHealth);
+        _healthbarSmooth.SetHealth(_currentHealth);
+    }
+
+    public void TakeDamage()
+    {
+        _currentHealth -= _damageAmount;
+
+        DisplayHealth();
+    }
+
+    public void Heal()
+    {
+        _currentHealth += _healingAmount;
+
+        DisplayHealth();
+    }
+}
---
+++ Assets/Scripts/HealthbarText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HealthbarText : MonoBehaviour
+{
+    private TMP_Text _healthText;
+    private float _maxHealth;
+
+    private void Awake()
+    {
+        _healthText = GetComponent<TMP_Text>();
+    }
+
+    public void SetMaxHealth(float maxHealth)
+    {
+        _maxHealth = maxHealth;
+    }
+
+    public void SetHealth(float health)
+    {
+        _healthText.text = ($"{health} / {_maxHealth}");
+    }
+}
